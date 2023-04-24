8e7541cda468ed2c98e16b2d676c7b4f93a1cc71
songtianyi <tianyi@saas3.io>
2023-04-24 20:25:13 +0800

    first commit

---
+++ pallets/court/src/lib.rs
@@ -0,0 +1,360 @@
+// Copyright (C) SaaS3.
+// SPDX-License-Identifier: Apache-2.0
+
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// 	http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+//! # Court Pallet
+//!
+//! The Court pallet provides a set of functions that can allow users to submit lawsuit
+//! and jury can vote the lawsuits.
+//!
+//! - [`Config`]
+//! - [`Call`]
+//!
+//! ## Overview
+//!
+//! The jury could vote to the lawsuits and approve or reject a lawsuit
+//!
+//!
+//! ### Terminology
+//!
+//! ## Interface
+//!
+//! ### Dispatchable Functions
+//!
+//! ## GenesisConfig
+//!
+//! The Court depends on the [`GenesisConfig`].
+
+#![cfg_attr(not(feature = "std"), no_std)]
+
+mod benchmarking;
+#[cfg(test)]
+mod tests;
+
+use codec::{Decode, Encode};
+use scale_info::TypeInfo;
+
+use sp_runtime::{traits::StaticLookup, RuntimeDebug};
+use sp_std::prelude::*;
+
+use frame_support::{
+	inherent::Vec,
+	traits::{Currency, Get, ReservableCurrency},
+	weights::Weight,
+};
+
+pub use pallet::*;
+
+pub type BalanceOf<T, I = ()> =
+	<<T as Config<I>>::Currency as Currency<<T as frame_system::Config>::AccountId>>::Balance;
+type AccountIdLookupOf<T> = <<T as frame_system::Config>::Lookup as StaticLookup>::Source;
+
+/// An index of a proposal. Just a `u32`.
+pub type ProposalIndex = u32;
+
+/// A submitted lawsuit
+#[cfg_attr(feature = "std", derive(serde::Serialize, serde::Deserialize))]
+#[derive(Encode, Decode, Clone, PartialEq, Eq, RuntimeDebug, TypeInfo)]
+pub struct Lawsuit<AccountId, Balance> {
+	/// The account proposing it.
+	plaintiff: AccountId,
+	/// The (total) amount that should be paid if the proposal is accepted.
+	value: Balance,
+	/// The account to whom the payment should be made if the proposal is accepted.
+	defendent: AccountId,
+	statement: Vec<u8>,
+	voters: Vec<AccountId>,
+	votes: Vec<bool>,
+	approved: bool,
+}
+
+#[frame_support::pallet]
+pub mod pallet {
+	use super::*;
+	use frame_support::pallet_prelude::*;
+	use frame_system::{ensure_root, pallet_prelude::*};
+
+	#[pallet::pallet]
+	#[pallet::without_storage_info]
+	pub struct Pallet<T, I = ()>(PhantomData<(T, I)>);
+
+	#[pallet::config]
+	pub trait Config<I: 'static = ()>: frame_system::Config {
+		/// The staking balance.
+		type Currency: Currency<Self::AccountId> + ReservableCurrency<Self::AccountId>;
+
+		/// The overarching event type.
+		type RuntimeEvent: From<Event<Self, I>>
+			+ IsType<<Self as frame_system::Config>::RuntimeEvent>;
+
+		/// The maximum number of approvals that can wait in the spending queue.
+		///
+		/// NOTE: This parameter is also used within the Bounties Pallet extension if enabled.
+		#[pallet::constant]
+		type MaxApprovals: Get<u32>;
+	}
+
+	/// Number of proposals that have been made.
+	#[pallet::storage]
+	#[pallet::getter(fn proposal_count)]
+	pub(crate) type ProposalCount<T, I = ()> = StorageValue<_, ProposalIndex, ValueQuery>;
+
+	/// Proposals that have been made.
+	#[pallet::storage]
+	#[pallet::getter(fn proposals)]
+	pub type Proposals<T: Config<I>, I: 'static = ()> = StorageMap<
+		_,
+		Twox64Concat,
+		ProposalIndex,
+		Lawsuit<T::AccountId, BalanceOf<T, I>>,
+		OptionQuery,
+	>;
+
+	/// Proposal indices that have been approved but not yet awarded.
+	#[pallet::storage]
+	#[pallet::getter(fn approvals)]
+	pub type Approvals<T: Config<I>, I: 'static = ()> =
+		StorageValue<_, BoundedVec<ProposalIndex, T::MaxApprovals>, ValueQuery>;
+
+	#[pallet::genesis_config]
+	pub struct GenesisConfig;
+
+	#[cfg(feature = "std")]
+	impl Default for GenesisConfig {
+		fn default() -> Self {
+			Self
+		}
+	}
+
+	#[cfg(feature = "std")]
+	impl GenesisConfig {
+		/// Direct implementation of `GenesisBuild::assimilate_storage`.
+		#[deprecated(
+			note = "use `<GensisConfig<T, I> as GenesisBuild<T, I>>::assimilate_storage` instead"
+		)]
+		pub fn assimilate_storage<T: Config<I>, I: 'static>(
+			&self,
+			storage: &mut sp_runtime::Storage,
+		) -> Result<(), String> {
+			<Self as GenesisBuild<T, I>>::assimilate_storage(self, storage)
+		}
+	}
+
+	#[pallet::genesis_build]
+	impl<T: Config<I>, I: 'static> GenesisBuild<T, I> for GenesisConfig {
+		fn build(&self) {}
+	}
+
+	#[pallet::event]
+	#[pallet::generate_deposit(pub(super) fn deposit_event)]
+	pub enum Event<T: Config<I>, I: 'static = ()> {
+		/// New proposal.
+		Proposed {
+			proposal_index: ProposalIndex,
+		},
+		/// We have ended a spend period and will now allocate funds.
+		Spending {
+			budget_remaining: BalanceOf<T, I>,
+		},
+		/// Some funds have been deposited.
+		Deposit {
+			value: BalanceOf<T, I>,
+		},
+		/// A new spend proposal has been approved.
+		SpendApproved {
+			proposal_index: ProposalIndex,
+			amount: BalanceOf<T, I>,
+			beneficiary: T::AccountId,
+		},
+		ProposalClosed {
+			lawsuit_id: u32,
+			approve: bool,
+		},
+		/// The inactive funds of the pallet have been updated.
+		UpdatedInactive {
+			reactivated: BalanceOf<T, I>,
+			deactivated: BalanceOf<T, I>,
+		},
+		VoteSubmitted {
+			lawsuit_id: u32,
+			voter: T::AccountId,
+			approve: bool,
+		},
+	}
+
+	/// Error for the treasury pallet.
+	#[pallet::error]
+	pub enum Error<T, I = ()> {
+		/// Proposer's balance is too low.
+		InsufficientProposersBalance,
+		/// No proposal or bounty at that index.
+		InvalidIndex,
+		/// Too many approvals in the queue.
+		TooManyApprovals,
+		/// The spend origin is valid but the amount it is allowed to spend is lower than the
+		/// amount to be spent.
+		InsufficientPermission,
+		/// Proposal has not been approved.
+		ProposalNotApproved,
+		ProposalAlreadyApproved,
+		ProposalNotFound,
+		LawsuitNotFound,
+		DuplicateVote,
+		StatementOverSize,
+	}
+
+	#[pallet::hooks]
+	impl<T: Config<I>, I: 'static> Hooks<BlockNumberFor<T>> for Pallet<T, I> {
+		/// ## Complexity
+		/// - `O(A)` where `A` is the number of approvals
+		fn on_initialize(_n: T::BlockNumber) -> Weight {
+			Weight::zero()
+		}
+	}
+
+	#[pallet::call]
+	impl<T: Config<I>, I: 'static> Pallet<T, I> {
+		/// Submit a lawsuit
+		///
+		/// ## Complexity
+		/// - O(1)
+		#[pallet::call_index(0)]
+		#[pallet::weight(Weight::zero())]
+		pub fn submit_sue(
+			origin: OriginFor<T>,
+			#[pallet::compact] value: BalanceOf<T, I>,
+			defendent: AccountIdLookupOf<T>,
+			statement: Vec<u8>,
+		) -> DispatchResult {
+			let proposer = ensure_signed(origin)?;
+			let defendent = T::Lookup::lookup(defendent)?;
+			ensure!(statement.len() < 512, Error::<T, I>::StatementOverSize);
+
+			let mut arr = [0u8; 512];
+
+			arr[..statement.len()].copy_from_slice(&&statement[..]);
+
+			let c = Self::proposal_count();
+			<ProposalCount<T, I>>::put(c + 1);
+			let proposal = Lawsuit {
+				plaintiff: proposer,
+				value,
+				defendent,
+				statement: statement.clone(),
+				voters: vec![],
+				votes: vec![],
+				approved: false,
+			};
+			<Proposals<T, I>>::insert(c, proposal);
+
+			Self::deposit_event(Event::Proposed { proposal_index: c });
+			Ok(())
+		}
+
+		#[pallet::call_index(1)]
+		#[pallet::weight(Weight::zero())]
+		pub fn vote_sue(origin: OriginFor<T>, lawsuit_id: u32, approve: bool) -> DispatchResult {
+			let voter = ensure_signed(origin)?;
+
+			let mut lawsuit = <Proposals<T, I>>::get(lawsuit_id)
+				.ok_or(Error::<T, I>::LawsuitNotFound)?
+				.clone();
+			<Proposals<T, I>>::remove(lawsuit_id);
+
+			// Ensure the voter hasn't voted before
+			ensure!(!lawsuit.voters.contains(&voter), Error::<T, I>::DuplicateVote);
+
+			// Add the voter and their vote to the lawsuit
+			//let mut voters = lawsuit.voters;
+			//let mut votes = lawsuit.votes;
+			lawsuit.voters.push(voter.clone());
+			lawsuit.votes.push(approve);
+			<Proposals<T, I>>::insert(lawsuit_id, lawsuit);
+
+			Self::deposit_event(Event::VoteSubmitted { lawsuit_id, voter, approve });
+			Ok(())
+		}
+
+		#[pallet::call_index(2)]
+		#[pallet::weight((Weight::zero(), DispatchClass::Operational))]
+		pub fn close(origin: OriginFor<T>, lawsuit_id: u32) -> DispatchResult {
+			// Only root members can close the lawsuit
+			let _root = ensure_root(origin)?;
+			let mut proposal = <Proposals<T, I>>::get(lawsuit_id)
+				.ok_or(Error::<T, I>::ProposalNotFound)?
+				.clone();
+			//let proposal = p.clone();
+			<Proposals<T, I>>::remove(lawsuit_id);
+
+			// Ensure that the proposal is not already approved
+			ensure!(!proposal.approved, Error::<T, I>::ProposalAlreadyApproved);
+
+			// Update the tally of votes
+			let vote_count = proposal.votes.iter().filter(|v| **v == true).count() as u32;
+			let voter_count = proposal.votes.len();
+			let approval_threshold = (voter_count as u32) * 3 / 4; // Simple majority
+			if vote_count >= approval_threshold {
+				// Proposal is approved, execute the proposal
+				T::Currency::transfer(
+					&proposal.defendent,
+					&proposal.plaintiff,
+					proposal.value,
+					frame_support::traits::ExistenceRequirement::KeepAlive,
+				)?;
+				proposal.approved = true;
+				<Proposals<T, I>>::insert(lawsuit_id, &proposal);
+				Approvals::<T, I>::try_append(lawsuit_id)
+					.map_err(|_| Error::<T, I>::TooManyApprovals)?;
+				Self::deposit_event(Event::ProposalClosed { lawsuit_id, approve: true });
+			} else {
+				proposal.approved = false;
+				<Proposals<T, I>>::insert(lawsuit_id, &proposal);
+				Self::deposit_event(Event::ProposalClosed { lawsuit_id, approve: false });
+			}
+
+			Ok(())
+		}
+
+		/// Force a previously approved lawsuit to be removed from the approval queue.
+		///
+		/// - `lawsuit_id`: The index of a lawsuit
+		///
+		/// ## Complexity
+		/// - O(A) where `A` is the number of approvals
+		///
+		/// Errors:
+		/// - `ProposalNotApproved`: The `lawsuit_id` supplied was not found in the approval queue,
+		/// i.e., the lawsuit has not been approved. This could also mean the lawsuit does not
+		/// exist altogether, thus there is no way it would have been approved in the first place.
+		#[pallet::call_index(3)]
+		#[pallet::weight((Weight::zero(), DispatchClass::Operational))]
+		pub fn remove_sue(
+			origin: OriginFor<T>,
+			#[pallet::compact] lawsuit_id: ProposalIndex,
+		) -> DispatchResult {
+			ensure_root(origin)?;
+
+			Approvals::<T, I>::try_mutate(|v| -> DispatchResult {
+				if let Some(index) = v.iter().position(|x| x == &lawsuit_id) {
+					v.remove(index);
+					Ok(())
+				} else {
+					Err(Error::<T, I>::ProposalNotApproved.into())
+				}
+			})?;
+
+			Ok(())
+		}
+	}
+}
---
+++ pallets/treasury/src/lib.rs
@@ -0,0 +1,299 @@
+// Copyright (C) SaaS3.
+// SPDX-License-Identifier: Apache-2.0
+
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// 	http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+//! # Treasury Pallet
+//!
+//! The Treasury pallet provides a "pot" of funds that can be managed by stakeholders in the system
+//! and a structure for making spending claims from this pot.
+//!
+//! - [`Config`]
+//! - [`Call`]
+//!
+//! ## Overview
+//!
+//! The Treasury Pallet itself provides the pot to store funds, and a means for jury to
+//! claim their rewards. The chain will need to provide a method (e.g.
+//! inflation, fees) for collecting funds.
+//!
+//! ### Terminology
+//! - **Beneficiary:** An account who will receive the funds from a rewards claim iff the claim is
+//!   approved.
+//! - **Pot:** Unspent funds accumulated by the treasury pallet.
+//!
+//! ## Interface
+//!
+//! ### Dispatchable Functions
+//!
+//! General spending/proposal protocol:
+//! - `receive` - Receiving funds from outside
+//! - `claim_rewards` - Jury claim their rewards
+//!
+//! ## GenesisConfig
+//!
+//! The Treasury pallet depends on the [`GenesisConfig`].
+
+#![cfg_attr(not(feature = "std"), no_std)]
+
+mod benchmarking;
+#[cfg(test)]
+mod tests;
+
+use codec::{Decode, Encode};
+use scale_info::TypeInfo;
+
+use sp_runtime::{
+	traits::{AccountIdConversion, Saturating},
+	RuntimeDebug,
+};
+use sp_std::prelude::*;
+
+use frame_support::{
+	traits::{Currency, ExistenceRequirement::KeepAlive, Get, ReservableCurrency},
+	weights::Weight,
+	PalletId,
+};
+
+pub use pallet::*;
+
+pub type BalanceOf<T, I = ()> =
+	<<T as Config<I>>::Currency as Currency<<T as frame_system::Config>::AccountId>>::Balance;
+
+/// An index of a record. Just a `u32`.
+pub type RecordIndex = u32;
+
+#[cfg_attr(feature = "std", derive(serde::Serialize, serde::Deserialize))]
+#[derive(Encode, Decode, Clone, PartialEq, Eq, RuntimeDebug, TypeInfo)]
+pub struct Record<AccountId, Balance> {
+	/// The account fund it
+	funder: AccountId,
+	/// The amount that funder send
+	value: Balance,
+	category_type: u32,
+}
+
+#[frame_support::pallet]
+pub mod pallet {
+	use super::*;
+	use frame_support::pallet_prelude::*;
+	use frame_system::pallet_prelude::*;
+
+	#[pallet::pallet]
+	#[pallet::without_storage_info]
+	pub struct Pallet<T, I = ()>(PhantomData<(T, I)>);
+
+	#[pallet::config]
+	pub trait Config<I: 'static = ()>: frame_system::Config + pallet_court::Config {
+		/// The staking balance.
+		type Currency: Currency<Self::AccountId> + ReservableCurrency<Self::AccountId>;
+
+		/// The overarching event type.
+		type RuntimeEvent: From<Event<Self, I>>
+			+ IsType<<Self as frame_system::Config>::RuntimeEvent>;
+
+		/// The treasury's pallet id, used for deriving its sovereign account ID.
+		#[pallet::constant]
+		type PalletId: Get<PalletId>;
+
+		/// The origin required for approving spends from the treasury outside of the proposal
+		/// process. The `Success` value is the maximum amount that this origin is allowed to
+		/// spend at a time.
+		type SpendOrigin: EnsureOrigin<Self::RuntimeOrigin, Success = BalanceOf<Self, I>>;
+	}
+
+	/// Number of proposals that have been made.
+	#[pallet::storage]
+	#[pallet::getter(fn record_count)]
+	pub(crate) type RecordCount<T, I = ()> = StorageValue<_, RecordIndex, ValueQuery>;
+
+	/// Proposals that have been made.
+	#[pallet::storage]
+	#[pallet::getter(fn records)]
+	pub type Records<T: Config<I>, I: 'static = ()> = StorageMap<
+		_,
+		Twox64Concat,
+		RecordIndex,
+		Record<T::AccountId, BalanceOf<T, I>>,
+		OptionQuery,
+	>;
+
+	#[pallet::genesis_config]
+	pub struct GenesisConfig;
+
+	#[cfg(feature = "std")]
+	impl Default for GenesisConfig {
+		fn default() -> Self {
+			Self
+		}
+	}
+
+	#[cfg(feature = "std")]
+	impl GenesisConfig {
+		/// Direct implementation of `GenesisBuild::assimilate_storage`.
+		#[deprecated(
+			note = "use `<GensisConfig<T, I> as GenesisBuild<T, I>>::assimilate_storage` instead"
+		)]
+		pub fn assimilate_storage<T: Config<I>, I: 'static>(
+			&self,
+			storage: &mut sp_runtime::Storage,
+		) -> Result<(), String> {
+			<Self as GenesisBuild<T, I>>::assimilate_storage(self, storage)
+		}
+	}
+
+	#[pallet::genesis_build]
+	impl<T: Config<I>, I: 'static> GenesisBuild<T, I> for GenesisConfig {
+		fn build(&self) {
+			// Create Treasury account
+			let account_id = <Pallet<T, I>>::account_id();
+			let min = <T as pallet::Config<I>>::Currency::minimum_balance();
+			if <T as pallet::Config<I>>::Currency::free_balance(&account_id) < min {
+				let _ = <T as pallet::Config<I>>::Currency::make_free_balance_be(&account_id, min);
+			}
+		}
+	}
+
+	#[pallet::event]
+	#[pallet::generate_deposit(pub(super) fn deposit_event)]
+	pub enum Event<T: Config<I>, I: 'static = ()> {
+		/// Some funds have been deposited.
+		Deposit {
+			value: BalanceOf<T, I>,
+		},
+		/// A new rewards claim has been approved.
+		ClaimApproved {
+			amount: BalanceOf<T, I>,
+			beneficiary: T::AccountId,
+		},
+		Spending {
+			budget_remaining: BalanceOf<T, I>,
+		},
+		/// Some funds have been allocated.
+		Awarded {
+			award: BalanceOf<T, I>,
+			account: T::AccountId,
+		},
+	}
+
+	/// Error for the treasury pallet.
+	#[pallet::error]
+	pub enum Error<T, I = ()> {
+		/// The spend origin is valid but the amount it is allowed to spend is lower than the
+		/// amount to be spent.
+		InsufficientPermission,
+		/// The remaining fund is less than the claimed amount
+		InsufficientFund,
+		/// Rewards claim has not been approved.
+		ClaimNotApproved,
+	}
+
+	#[pallet::hooks]
+	impl<T: Config<I>, I: 'static> Hooks<BlockNumberFor<T>> for Pallet<T, I> {
+		/// ## Complexity
+		/// - `O(A)` where `A` is the number of approvals
+		fn on_initialize(_n: T::BlockNumber) -> Weight {
+			Weight::zero()
+		}
+	}
+
+	#[pallet::call]
+	impl<T: Config<I>, I: 'static> Pallet<T, I> {
+		/// Receive funds and save to treasury pot.
+		///
+		/// - `origin`: Must be `SpendOrigin` with the `Success` value being at least `amount`.
+		/// - `amount`: The amount to be transferred from origin to the treasury pot.
+		/// - `category_type`: The source type of funds
+		///
+		#[pallet::call_index(0)]
+		#[pallet::weight(Weight::zero())]
+		pub fn receive(
+			origin: OriginFor<T>,
+			#[pallet::compact] amount: BalanceOf<T, I>,
+			category_type: u32,
+		) -> DispatchResult {
+			let sender = ensure_signed(origin.clone())?;
+			let max_amount = T::SpendOrigin::ensure_origin(origin)?;
+			ensure!(amount <= max_amount, Error::<T, I>::InsufficientPermission);
+			let to = Self::account_id();
+			<T as pallet::Config<I>>::Currency::transfer(&sender, &to, amount, KeepAlive)?;
+
+			// TODO
+			// record funds
+			let c = Self::record_count();
+			<RecordCount<T, I>>::put(c + 1);
+			let record = Record { funder: sender, value: amount, category_type };
+			<Records<T, I>>::insert(c, record);
+
+			Self::deposit_event(Event::Deposit { value: (amount) });
+			Ok(())
+		}
+
+		/// Jury claim their rewards. This call will transfer claimed value to beneficiary
+		///
+		/// - `amount`: The amount to be tranferred to Origin
+		///
+		/// ## Complexity
+		///
+		/// Errors:
+		/// - `ClaimNotApproved`: The `origin` supplied was not found in the voting list.
+		#[pallet::call_index(1)]
+		#[pallet::weight(Weight::zero())]
+		pub fn claim_rewards(
+			origin: OriginFor<T>,
+			#[pallet::compact] amount: BalanceOf<T, I>,
+		) -> DispatchResult {
+			let beneficiary = ensure_signed(origin)?;
+			let budget_remaining = Self::pot();
+			ensure!(amount <= budget_remaining, Error::<T, I>::InsufficientFund);
+			Self::deposit_event(Event::Spending { budget_remaining });
+
+			let account_id = Self::account_id();
+
+			// TODO
+			// fetch ended lawsuit
+			// calculate rewards - claimed rewards
+			//
+
+			<T as pallet::Config<I>>::Currency::transfer(
+				&account_id,
+				&beneficiary,
+				amount,
+				KeepAlive,
+			)?;
+			Self::deposit_event(Event::Awarded { award: amount, account: beneficiary });
+
+			Ok(())
+		}
+	}
+}
+
+impl<T: Config<I>, I: 'static> Pallet<T, I> {
+	// Add public immutables and private mutables.
+
+	/// The account ID of the treasury pot.
+	///
+	/// This actually does computation. If you need to keep using it, then make sure you cache the
+	/// value and only call this once.
+	pub fn account_id() -> T::AccountId {
+		T::PalletId::get().into_account_truncating()
+	}
+
+	/// Return the amount of money in the pot.
+	// The existential deposit is not part of the pot so treasury account never gets deleted.
+	pub fn pot() -> BalanceOf<T, I> {
+		<T as pallet::Config<I>>::Currency::free_balance(&Self::account_id())
+			// Must never be less than 0 but better be safe.
+			.saturating_sub(<T as pallet::Config<I>>::Currency::minimum_balance())
+	}
+}
